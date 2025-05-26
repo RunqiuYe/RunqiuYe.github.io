f3440ca984d71e66ee6218960d98312802736b67
RunqiuYe <102471070+RunqiuYe@users.noreply.github.com>
2025-05-26 22:09:02 +0000

    Deploying to gh-pages from @ RunqiuYe/RunqiuYe.github.io@08a23bb0a5a54e5615b3812a745574688f0d12eb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -52,53 +52,13 @@ ninja.data = [{
             window.location.href = "/blog/2025/numdp/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-start-building-this-website",
+      },{id: "news-start-building-this-website",
           title: 'Start building this website!',
           description: "",
           section: "News",},{id: "news-finish-setting-up-the-blog-and-repositories-section",
           title: 'Finish setting up the blog and repositories section!',
           description: "",
           section: "News",},{
-        id: 'social-email',
-        title: 'email',
-        section: 'Socials',
-        handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
